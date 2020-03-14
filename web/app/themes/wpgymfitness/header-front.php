00720b3a631489074607d72f60c8485890f04d22
Lawrence <Caulker>
2020-03-14 16:01:55 -0400

    created alternate header-front.php and centered hero text and content

--- web/app/themes/wpgymfitness/header-front.php
+++ web/app/themes/wpgymfitness/header-front.php
@@ -12,7 +12,6 @@
   <header class="site-header">
     <div class="container header-grid">
       <div class="navigation-bar">
-
         <div class="logo">
           <a href="<?php echo home_url(); ?>">
             <img src="<?php echo get_template_directory_uri() . "/images/logo.svg"; ?>" alt="Site Logo">
@@ -28,8 +27,12 @@
           ];
           wp_nav_menu($args);
         ?>
-
       </div><!-- /end .navigation-bar -->
+
+      <div class="tagline text-center">
+        <h1><?php the_field('hero_tagline'); ?></h1>
+        <p><?php the_field('hero_content'); ?></p>
+      </div>
+
     </div><!-- /end .container .header-grid -->
   </header>
-  
